d48b70a5f12060f1987240cd40c214bf75eac411
hongqi zhang <hzhan193@asu.edu>
2022-11-25 22:16:24 -0600

    Initial commit

---
+++ src/main/java/com/apple/hzhan193/Client.java
@@ -0,0 +1,13 @@
+package com.apple.hzhan193;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
