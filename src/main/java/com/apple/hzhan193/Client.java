4c521709bc8fcfd7849caf59f1632eeab3b3bc94
hongqi zhang <hzhan193@asu.edu>
2022-11-25 22:17:58 -0600

    socket client

--- src/main/java/com/apple/hzhan193/Client.java
+++ src/main/java/com/apple/hzhan193/Client.java
@@ -1,13 +1,31 @@
 package com.apple.hzhan193;
 
+import java.io.IOException;
+import java.io.OutputStream;
+import java.net.Socket;
+import java.nio.charset.Charset;
+import java.nio.charset.StandardCharsets;
+
 /**
  * Hello world!
  *
  */
-public class App 
+public class Client
 {
-    public static void main( String[] args )
-    {
-        System.out.println( "Hello World!" );
+    public static void main( String[] args ) {
+        String host = "127.0.0.1";
+        int port = 8000;
+        try {
+            Socket socket = new Socket(host, port);
+            System.out.println("connecting to server...");
+
+            String message = "this is the message sent from client";
+            OutputStream outputStream = socket.getOutputStream();
+            outputStream.write(message.getBytes("UTF-8"));
+            outputStream.close();
+            socket.close();
+        }catch(IOException ex) {
+            System.out.println(ex.getMessage());
+        }
     }
 }
